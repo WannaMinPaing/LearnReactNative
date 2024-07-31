85c79e220131a6f668f456103d086aa89775c689
WannaMinPaing <paingwanna@gmail.com>
2024-07-31 08:33:43 +0630

    slove className error

--- app/index.tsx
+++ app/index.tsx
@@ -4,12 +4,13 @@ import { View, Text } from "react-native";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-black">
+    <View className="flex-1 items-center justify-center bg-white">
       <StatusBar style="auto" />
-      <Text> My Index App232</Text>
+      <Text className="text-3xl"> My Index App21132</Text>
       <Link href="/profile" style={{ color: "blue" }}>
         Go to Profile
       </Link>
     </View>
   );
 }
+// 29:45
